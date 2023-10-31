5468a52b73f87d49f7377e4648c6e95614f8fafa
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2023-10-31 10:38:59 +0530

    Time: 9 ms (78.31%), Space: 11.3 MB (79.91%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
 
     int solve(int k, int n, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        vector<vector<int>> prev(2, vector<int>(k+1, 0));
+        vector<vector<int>> curr(2, vector<int>(k+1, 0));
         for(int i= n-1; i>=0; i--) {
             for(int buy = 0; buy<=1; buy++) {
                 for(int cap = 1; cap<=k; cap++) {
                     if(buy == 1) {
-                        dp[i][buy][cap] = max(-prices[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+                        curr[buy][cap] = max(-prices[i] + prev[0][cap], prev[1][cap]);
                     } else {
-                        dp[i][buy][cap] = max(prices[i] + dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                        curr[buy][cap] = max(prices[i] + prev[1][cap-1], prev[0][cap]);
                     }
                 }
             }
+            prev = curr;
         }
-        return dp[0][1][k];
+        return prev[1][k];
     }
 
     int maxProfit(int k, vector<int>& prices) {
