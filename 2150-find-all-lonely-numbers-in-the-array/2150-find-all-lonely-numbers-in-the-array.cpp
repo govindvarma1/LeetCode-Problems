a7e369b483f715968b43ebe6135de799eacf15e9
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-01-19 11:24:07 +0530

    Time: 307 ms (71.27%), Space: 167.5 MB (74.22%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
 
-    int binarySearch(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() -1, mid;
-        while(start <= end) {
-            mid = (start + end) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    bool isRepetative(vector<int>& nums, int target) {
-        int ind = binarySearch(nums, target), n =nums.size();
-        if(ind > 0 && nums[ind-1] == nums[ind]) {
-            return true;
-        } 
-        if(ind < n-1 && nums[ind+1] == nums[ind]) {
-            return true;
-        }
-        return false;
-    }
-
+    //HashMap Approach
     vector<int> findLonely(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<int> ans;
+        vector<int> ans; int n = nums.size();
+        unordered_map<int, int> hashMap;
         for(int i=0; i<n; i++) {
-            if(!isRepetative(nums, nums[i]) && (i==0 || nums[i-1] != nums [i] -1) && (i==n-1 || nums[i+1] != nums[i]+1)) {
-                ans.push_back(nums[i]);
+            hashMap[nums[i]]++;
+        }
+        for(int i =0; i<n; i++) {
+            if(hashMap[nums[i]] == 1) {
+                if(hashMap.find(nums[i] + 1) == hashMap.end() && hashMap.find(nums[i] - 1) == hashMap.end()) {
+                    ans.push_back(nums[i]);
+                }
             }
-        }               
+        }
         return ans;
     }
 };
