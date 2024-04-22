794ffcff180b62f33ebad6ebd73b1e42d0ddc220
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-04-22 15:23:48 +0530

    Time: 8 ms (98.03%), Space: 76.1 MB (50.50%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* Reverse(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        ListNode* temp = nullptr;
+        while(curr != nullptr) {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* num1 = l1;
+        ListNode* num2 = l2;
+        ListNode* addedList = new ListNode;
+        ListNode* ans = addedList;
+        int carry = 0;
+        while(num1 != nullptr && num2 != nullptr) {
+            int value = num1->val + num2->val + carry;
+            ListNode* temp = new ListNode(value%10);
+            ans->next = temp;
+            ans = ans->next;
+            num1 = num1->next;
+            num2 = num2->next;
+            carry = value/10;
+        }        
+        while(num1 != nullptr) {
+            int value = num1->val +carry;
+            ListNode* temp = new ListNode(value%10);
+            ans->next = temp;
+            ans = ans->next;
+            num1 = num1->next;
+            carry = value/10;
+        }
+        while(num2 != nullptr) {
+            int value = num2->val +carry;
+            ListNode* temp = new ListNode(value%10);
+            ans->next = temp;
+            ans = ans->next;
+            num2 = num2->next;
+            carry = value/10;
+        }
+        if(carry == 1) {
+            ListNode* temp = new ListNode(1);
+            ans->next = temp;
+        }
+        return addedList->next;
+    }
+};
