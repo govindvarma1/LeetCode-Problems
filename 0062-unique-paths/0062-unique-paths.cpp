5c3483d4a2a2019abd5585c6deaff58c6217ad94
Govinda Varma Kallepalli <98372421+govindvarma1@users.noreply.github.com>
2025-05-28 13:03:42 +0530

    Time: 1 ms (24.71%), Space: 9.5 MB (8.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>& dp, int n, int m) {
-        dp[0][0]=1;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(i!=0 || j!=0) {
-                    if(i>0) {
-                        dp[i][j]+=dp[i-1][j];
-                    } 
-                    if(j>0) {
-                        dp[i][j]+=dp[i][j-1];
-                    }
-                }   
-            }
+    bool isPossible(vector<vector<int>>& vis, int row, int col, int m, int n) {
+        if(row >= 0 && row < m && col >= 0 && col < n && vis[row][col] == -1) {
+            return true;
         }
-        return dp[n-1][m-1];
-        
+        return false;
+    }
+
+    int solve(vector<vector<int>>& vis, vector<vector<int>>& dp, int row, int col) {
+        int m = vis.size(), n = vis[0].size();
+        if(row == m - 1 && col == n - 1) {
+            return 1;
+        }
+        vis[row][col] = 1;
+        int res = 0;
+        if(dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        if(isPossible(vis, row + 1, col, m, n)) {
+            res += solve(vis, dp, row + 1, col);
+        } 
+        if(isPossible(vis, row, col + 1, m, n)) {
+            res += solve(vis, dp, row, col + 1);
+        }
+        vis[row][col] = -1;
+        return dp[row][col] = res;
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        return solve(dp, n, m);                 
+        vector<vector<int>> vis(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(vis, dp, 0, 0);
     }
 };
